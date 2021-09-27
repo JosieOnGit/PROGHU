4f057f507dae2cb84630c5315de4707025cbc667
Josie <91462966+BlackCatMS@users.noreply.github.com>
2021-09-27 12:35:05 +0200

    Add files via upload

---
+++ MESS/GitHub_testing.py
@@ -0,0 +1,12 @@
+
+def stopGithub(stopGithub):
+    github = False
+    print("That's all, folks!")
+
+
+github = True
+while github is True:
+    print("We brokey!")
+    github = False
+
+stopGithub(stopGithub)

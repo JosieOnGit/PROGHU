b55317be610bbb7468515ee4e94b2d226ee31af1
Josie <theblackcatms@gmail.com>
2021-09-30 16:08:16 +0200

    Module 2 completed

--- PROJA/Module_2.py
+++ PROJA/Module_2.py
@@ -1,1 +1,30 @@
 
+def review():
+    messageDB = open("tweet_list.txt", "r+")
+    approvedTweets = open("approved_tweets.txt", "a")
+    rejectedTweets = open("rejected_tweets.txt", "a")
+    readMessage = messageDB.readlines()
+    for line in readMessage:
+        message = line.strip().split(sep=", ")
+        print("\"" + message[-1] + "\"")
+        print("Please check the above message for any profanity, reliability, and others.")
+        approvalQ = input("Do you approve this message? (Y/N) ")
+        if approvalQ.lower() == "y":
+            print("You've approved this message: " + message[-1] + "\n")
+            approvedTweets.write("Approved, " + line)
+        else:
+            print("You've rejected this message: " + message[-1] + "\n")
+            rejectedTweets.write("Rejected, " + line)
+
+    messageDB.close()
+    approvedTweets.close()
+
+
+def cleanup():
+    messageDB = open("tweet_list.txt", "w")
+    messageDB.write("")
+    messageDB.close()
+
+
+review()
+cleanup()

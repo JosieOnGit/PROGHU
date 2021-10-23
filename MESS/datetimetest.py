1adffdcf9ed34d1cd02cb271cda638b9507dc895
Josie <theblackcatms@gmail.com>
2021-10-23 15:09:55 +0200

    Module 2 updated

---
+++ MESS/datetimetest.py
@@ -0,0 +1,7 @@
+
+import datetime as dt
+
+date = dt.datetime.now().date()
+time = dt.datetime.now().time()
+print(date)
+print(time)
--- PROJA/Module_2.py
+++ PROJA/Module_2.py
@@ -1,14 +1,24 @@
 
+import datetime as dt
+
+
 def review():
     messageDB = open("tweet_list.txt", "r+")
     approvedTweets = open("approved_tweets.txt", "a")
     rejectedTweets = open("rejected_tweets.txt", "a")
     readMessage = messageDB.readlines()
+
+    date = dt.datetime.now().date()
+    time = dt.datetime.now().time()
+
+    superuser = input("Please enter your last name: ")
+    reviewLine = f"The following messages have been reviewed by {superuser} on {date} at {time}:\n"
+
     for line in readMessage:
         message = eval(line)["message"]
         print(f"\"{message}\"")
         print("Please check the above message for any profanity, reliability, etc.")
-        approvalQ = input("Do you approve this message? (Y/N) ")
+        approvalQ = input("Do you approve this message? (Y/N): ")
         if approvalQ.lower() == "y":
             print(f"You've approved this message: \"{message}\"\n")
             approvedTweets.write(line)
@@ -16,7 +26,6 @@ def review():
             print(f"You've rejected this message: \"{message}\"\n")
             rejectedTweets.write(line)
 
-    print("That was all, thank you for reviewing the new messages!")
     messageDB.close()
     approvedTweets.close()
     rejectedTweets.close()
